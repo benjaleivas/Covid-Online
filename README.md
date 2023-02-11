c2bea51c7f5f681e397321db9aa2d68c9f5ea3e3
claireboyd <claire.k.boyd@gmail.com>
2023-02-11 17:58:05 -0600

    added TODOs, fixed bug

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Overview of project...[add more here]
 
 To re-produce our data collection, you will need to get an API key for the analytics.us.gov API. Here are the steps to make sure you're set up correctly in order to re-produce our work:
 
-* get key by filling out [the form](https://open.gsa.gov/api/dap/) under **Getting Started**.
+* get personalized key by filling out [the form](https://open.gsa.gov/api/dap/) under **Getting Started**.
 
-* after retrieving a personalized API key from your email address, run the following in an ipython3 session:
+* after retrieving a personalized API key from your email, run the following in an ipython3 session:
 
 ```python
 import os
--- collect/analytics_data.py
+++ collect/analytics_data.py
@@ -1,11 +1,18 @@
 import requests
 import json
 import os
-from utils import report_agency, report_name
+from .utils import report_agency, report_name
 
 #!!! Set this in your environment or the code won't run!!! 
-# (see README.md for instructions)
+# (see README.md for instructions on how to get your own API key)
 key = os.environ['ANALYTICS_API_KEY']
+
+# decompose further, breaking apart the function below into two parts
+
+# TODO: function to get a FULL day of data
+
+# TODO: function with date range that sends each day to previous function and aggregates
+
 
 def get_analytics_by_agency(agency, date_range, report_type, max_pulls=100, limit=10000):
     ''''
@@ -13,11 +20,19 @@ def get_analytics_by_agency(agency, date_range, report_type, max_pulls=100, limi
 
     Inputs:
         agency (str): filter agency
-        data_range (tup): tuple containing start and end of time frame.
-            Expects YYYY-MM-DD.
+        data_range (tup): tuple of strs containing start and end of time frame.
+            Expects YYYY-MM-DD. For example: ("2020-02-01", "2020-02-02")
         report_type: type of analytics report
         max_pull (int): optional, number of pulls made at one time
         limit (int): optional, limit to API request 
+    
+    Returns: list of dictionaries (the content of the dictionaries depends 
+      on the type of report selected, but may include:
+            id (int)
+            date (str): in format YYYY-MM-DD
+            report_name (str)
+            domain (str)
+            visits (int)
         '''
 
     start_date, stop_date = date_range
@@ -35,10 +50,7 @@ def get_analytics_by_agency(agency, date_range, report_type, max_pulls=100, limi
         pull_count += 1
         
         if curr_response.json()[-1]['date'] == stop_date:
-            
             return results
-        
+    
+    # added second return statement bc was returning none if it didnt hit the stop data conditional
     return results
-    
-    
-
