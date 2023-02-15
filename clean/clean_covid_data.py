049e8eaaaf0553d0af96320e61934a051fb2d9a7
Claire Boyd <claire.k.boyd@gmail.com>
2023-02-15 21:04:19 +0000

    added nyt function

---
+++ clean/clean_covid_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def add_column_for_daily(data_type):
+    """
+    """
+    pass
--- collect/covid_data.py
+++ collect/covid_data.py
@@ -1,74 +1,22 @@
 import pandas as pd
-from sodapy import Socrata
 import json
 import requests
-from .utils import cdc_report_name
 
-def get_jh_data(data_type):
-    """
-    Retrieves COVID-19 data from the Center for Systems Science and 
-      Engineering (CSSE) at John Hopkins University by data_type
+def get_nyt_data():
+  """
+  Retrieves COVID-19 data from NYTimes Github, which includes date,
+    total cases and total deaths.
 
-    Inputs: 
-      data_type (str): either "cases", "deaths", "vaccinations", or "tests"
+  Inputs: None
 
-    Returns: pandas Dataframe with all cases or deaths by county
-    """
-    data_types = ["cases", "deaths", "vaccinations", "tests"]
-    
-    assert data_type in data_types, "data_type input not valid. Must be 'cases', 'deaths', 'vaccinations', or 'tests'."
-    
-    # CASES AND DEATHS
-    if data_type in ["cases", "deaths"]:
-        # use "cases" as input to function, but "confirmed" is in url string
-        if data_type == "cases":
-            data_type = "confirmed"
+  Returns: pandas Dataframe with three columns: date, total cases, 
+    and total deaths.
+  """
+  url = f"https://github.com/nytimes/covid-19-data/raw/master/us.csv"
 
-        url = f"https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_{data_type}_US.csv"
-
-    # VACINATIONS AND TESTS
-    if data_type in ["vaccinations", "tests"]:
-        if data_type == "vaccinations":
-            url_string = "testing_data/county_time_series_covid19_US"
-        else:
-            #if data_type == tests
-            url_string = "vaccine_data/us_data/time_series/time_series_covid19_vaccine_us"
-        
-        url = f"https://github.com/govex/COVID-19/raw/master/data_tables/{url_string}.csv"    
-    
-    # get dataframe from url - but can turn this into a dict if needed
-    df = pd.read_csv(url)
-
-    return df
-
-#TODO: might need to separate CDC data grab into multiple functions bc the 
-   # API filters for vacctinaions and cases and deaths are different
-   # or alternatively standardize them a bit better in the call
-
-def get_cdc_data(report_type, start_date, end_date):
-    """
-
-    Inputs:
-      report_type (str) - options are "vaccinations" or "cases and deaths"
-      start_date (str), expects YYYY-MM-DD
-      end_date (str), expects YYYY-MM-DD
-    
-    Returns: 
-
-    """
-    # needed for accessing cdc data (don't need API token for public data)
-    client = Socrata("data.cdc.gov", None)
-    
-    results = client.get(
-        cdc_report_name[report_type],
-        where= f"start_date={start_date}"
-        #where= f"end_date={end_date}"
-    )
-
-    # Convert to pandas DataFrame
-    #results_df = pd.DataFrame.from_records(results)
-    
-    return results
+  df = pd.read_csv(url)
+  
+  return df
 
 
 def get_bls_data(start_year, end_year):
@@ -95,8 +43,56 @@ def get_bls_data(start_year, end_year):
 
 # outputs - where to save these files? a data folder locally or should we create a database?
 
+
+# unemployment = get_bls_data("2020", "2022")
+# cases_deaths = get_nyt_data()
+
+
+
+
+
+
+
+## NOT SURE IF WE NEED THIS ANYMORE
+
+# def get_jh_data(data_type):
+#     """
+#     Retrieves COVID-19 data from the Center for Systems Science and 
+#       Engineering (CSSE) at John Hopkins University by data_type
+
+#     Inputs: 
+#       data_type (str): either "cases", "deaths", "vaccinations", or "tests"
+
+#     Returns: pandas Dataframe with all cases or deaths by county
+#     """
+#     data_types = ["cases", "deaths", "vaccinations", "tests"]
+    
+#     assert data_type in data_types, "data_type input not valid. Must be 'cases', 'deaths', 'vaccinations', or 'tests'."
+    
+#     # CASES AND DEATHS
+#     if data_type in ["cases", "deaths"]:
+#         # use "cases" as input to function, but "confirmed" is in url string
+#         if data_type == "cases":
+#             data_type = "confirmed"
+
+#         url = f"https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_{data_type}_US.csv"
+
+#     # VACINATIONS AND TESTS
+#     if data_type in ["vaccinations", "tests"]:
+#         if data_type == "vaccinations":
+#             url_string = "testing_data/county_time_series_covid19_US"
+#         else:
+#             #if data_type == tests
+#             url_string = "vaccine_data/us_data/time_series/time_series_covid19_vaccine_us"
+        
+#         url = f"https://github.com/govex/COVID-19/raw/master/data_tables/{url_string}.csv"    
+    
+#     # get dataframe from url - but can turn this into a dict if needed
+#     df = pd.read_csv(url)
+
+#     return df
+
 # jh_deaths = get_jh_data("deaths")
 # jh_cases = get_jh_data("cases")
 # jh_vaccinations = get_jh_data("vaccinations")
 # jh_tests = get_jh_data("tests")
-# unemployment = get_bls_data("2020", "2022")
