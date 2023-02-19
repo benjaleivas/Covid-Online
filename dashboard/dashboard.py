7243e11f90320fb269ece855fc7aed51af876b9d
Raul Santiago Castellanos <raulcastellanos@linux5.cs.uchicago.edu>
2023-02-19 16:54:28 -0600

    finish second graph

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -16,16 +16,21 @@ def get_data ():
 
 # Build your components
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-mytext = dcc.Markdown(children="# Are Americans Looking to .gov Websites for Health Information?")
-myparagraph = dbc.Row([
+mytext_1 = dcc.Markdown(children="# How was unemployment during covid?", style={"color": "#005aae"})
+myparagraph_1 = dbc.Row([
     dbc.Col(html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac pulvinar lectus, in efficitur ligula. Nulla facilisi. Donec nec est porttitor, malesuada odio quis, lobortis velit. Fusce finibus ullamcorper nulla, et tincidunt lectus porttitor sed. Vivamus dictum dictum eleifend. Cras nec nunc at lectus ornare posuere. Praesent tempor porta erat, et finibus risus. In et ornare enim. Aliquam erat volutpat. Integer viverra augue vitae erat ultricies gravida. Etiam pellentesque ipsum in tortor finibus porttitor."), width=10)
 ])
 
+mytext_2 = dcc.Markdown(children="# How was unemployment during covid? Barplot", style={"color": "#005aae"})
+myparagraph_2 = dbc.Row([
+    dbc.Col(html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac pulvinar lectus, in efficitur ligula. Nulla facilisi. Donec nec est porttitor, malesuada odio quis, lobortis velit. Fusce finibus ullamcorper nulla, et tincidunt lectus porttitor sed. Vivamus dictum dictum eleifend. Cras nec nunc at lectus ornare posuere. Praesent tempor porta erat, et finibus risus. In et ornare enim. Aliquam erat volutpat. Integer viverra augue vitae erat ultricies gravida. Etiam pellentesque ipsum in tortor finibus porttitor."), width=10)
+])
 # Get the data
 df = get_data()
 
+
 # Add time series graph
-fig = {
+fig1 = {
     'data': [
         {'x': df['date'], 'y': df['unemployment_rate'], 'type': 'line', 'name': 'Value'},
     ],
@@ -35,7 +40,20 @@ fig = {
         'yaxis': {'title': 'Value'}
     }
 }
-mygraph = dcc.Graph(figure=fig)
+mygraph1 = dcc.Graph(figure=fig1)
+
+# Add barplot graph
+fig2 = {
+    'data': [
+        {'x': df['date'], 'y': df['unemployment_rate'], 'type': 'bar', 'name': 'Value'},
+    ],
+    'layout': {
+        'title': 'Barplot Graph',
+        'xaxis': {'title': 'Date'},
+        'yaxis': {'title': 'Value'}
+    }
+}
+mygraph2 = dcc.Graph(figure=fig2)
 
 # Customize your own Layout
 # Customize your own Layout
@@ -47,19 +65,19 @@ app.layout = dbc.Container(
                 "height": "100vh",
                 "background-color": "#005aae",
                 "color": "white",
-                "font-size": "3rem"
+                "font-size": "6rem"
             },
             children=[
                 dbc.Col(
                     html.H1(
                         "Are Americans Looking to .gov Websites for Health Information?",
-                        style={"text-align": "center", "font-size": "1rem", "margin-bottom": "0.5rem"}
+                        style={"text-align": "center", "font-size": "6rem", "margin-bottom": "6rem"}
                     ),
                     width=12
                 ),
                 dbc.Col(
                     html.H2(
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et ipsum nisl. Maecenas tincidunt turpis ut tellus efficitur convallis. Vestibulum vestibulum dui at justo rutrum molestie. Maecenas quis eleifend sapien. Sed ultrices nunc at neque consequat, eu ullamcorper nisi sodales. Mauris accumsan gravida scelerisque. Nunc sodales dui in.",
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac pulvinar lectus, in efficitur ligula. Nulla facilisi. Donec nec est porttitor, malesuada odio quis, lobortis velit.",
                         style={"text-align": "center", "font-size": "1rem", "color": "#ffffff", "margin-bottom": "0.5rem"}
                     ),
                     width=12
@@ -68,13 +86,16 @@ app.layout = dbc.Container(
             justify="center",
             align="center"
         ),
-        mytext,
-        myparagraph, 
-        mygraph
+        mytext_1,
+        myparagraph_1, 
+        mygraph1, 
+        mytext_2, 
+        myparagraph_2, 
+        mygraph2
     ]
 )
 
 
 # Run app
 if __name__=='__main__':
-    app.run_server(port=8051)
+    app.run_server(port=8052)
