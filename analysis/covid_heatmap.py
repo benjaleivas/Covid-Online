79061e6c643ff2456993a0923416173148aa67c2
Benjamin Leiva <bleiva@linux5.cs.uchicago.edu>
2023-02-26 21:51:31 -0600

    Line and calmap for first slide

--- analysis/covid_heatmap.py
+++ analysis/covid_heatmap.py
@@ -2,12 +2,13 @@ import numpy as np
 import pandas as pd
 from plotly_calplot import calplot  # pip install plotly-calplot
 
-#Load COVID data
-data = pd.read_csv('data/cleaned_covid_data.csv', usecols=[1,2,3,4,5])
+#Load merged data
+data = pd.read_csv('data/merged_test_dataset.csv', 
+                   usecols=['date', 'visits', 'cases', 'daily_cases', 'deaths', 'daily_deaths'])
 
 #Transform data
 data['date'] = pd.to_datetime(data['date'])
-#data = data[data['date'].dt.year == 2022]                       #filter year
+#data = data[data['date'].dt.year == 2022]                      #filter year
 #data['daily_cases_diff'] = data.daily_cases.diff().fillna(0)   #differences
 #data = data[['date', 'daily_cases']]                           #relevant vars
 
@@ -16,8 +17,9 @@ fig = calplot(
          data,
          x="date",
          y="daily_cases",
-         years_title=True,
+         years_title=False,
          showscale=True,
          colorscale='reds'
 )
+
 fig.show()
