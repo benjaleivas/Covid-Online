d7de14ad2f9daa5adffd14f9bf9707fc29943a41
Benjamin Leiva <bleiva@linux2.cs.uchicago.edu>
2023-03-04 13:09:04 -0600

    Added events to HHS visits

--- happy_app/collect/utils.py
+++ happy_app/collect/utils.py
@@ -53,21 +53,6 @@ REPORT_NAME = {
     "DEVICE": "device",
 }
 
-
-KEY_DATES_2020 = {
-    "2020-03-13": "COVID-19 Declared National Emergency",
-    "2020-05-08": "FDA Authorizes First COVID-19 Test",
-    "2020-12-11": "FDA Authorizes Pfizer Vaccine",
-}
-
-KEY_DATES_2021 = {
-    "2021-03-08": "CDC Approves Safe Gathering for Vaccinated Individuals",
-    "2021-07-27": "CDC Recommends Masks Due to Delta Variant",
-    "2021-11-19": "Omnicron Surge Begins; CDC Urges Booster Shots",
-}
-
-KEY_DATES_2022 = {"2022-01-24": "Omnicron accounts for 99\% of COVID-19 Cases"}
-
 KEY_DATES = {
     "2020-03-13": "COVID-19 Declared National Emergency",
     "2020-05-08": "FDA Authorizes First COVID-19 Test",
