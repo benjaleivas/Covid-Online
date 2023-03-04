844daaadc8df5c9ac1af141430d9ede47ace4ed5
Benjamin Leiva <bleiva@linux2.cs.uchicago.edu>
2023-03-04 13:04:55 -0600

    Added events to HHS visits

--- happy_app/analysis/hhs_visits.py
+++ happy_app/analysis/hhs_visits.py
@@ -1,9 +1,12 @@
+import datetime
 import pandas as pd
 from dash import dcc
 import plotly.graph_objects as go
+from datetime import datetime as dt
 from plotly.subplots import make_subplots
+from happy_app.collect.utils import KEY_DATES
 
-def plot_visits_vs_2019(year):
+def plot_hhs_visits(year):
     """
     Plots trend of visits to HHS's website for 'year' relative to 2019, as well
     as the difference in visits between both years.
@@ -20,13 +23,6 @@ def plot_visits_vs_2019(year):
     diff['visits'] = post.visits - prev.visits
     datasets = [post, prev, diff]
 
-    #Create figure
-    fig = make_subplots(rows=1,
-                        cols=1,
-                        x_title=None,
-                        y_title=None,
-                        subplot_titles=None)
-
     #Define layout
     layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)', 
                        plot_bgcolor='rgba(0,0,0,0)',
@@ -38,11 +34,13 @@ def plot_visits_vs_2019(year):
                        yaxis_title=None, 
                        xaxis_title=None)
 
-    #Add traces
+    #Define lines formats
     line_types = ['solid', 'solid', 'dash']
     line_colors = ['#1E90FF', '#C1CDCD', '#CD2626']
     line_names = [f'{year}', 'Baseline (2019)', 'Difference with baseline']
 
+    #Create figure
+    fig = go.Figure()
     for idx in range(len(datasets)):
         fig.add_trace(go.Scatter(x = datasets[idx]['week'],
                                  y = datasets[idx]['visits'],
@@ -52,19 +50,16 @@ def plot_visits_vs_2019(year):
                                            color=line_colors[idx], 
                                            dash=line_types[idx])))
 
-    #Add events (Create dictionary for this in 'data' folder)
-    # event_2020_1 = {'year': 2021, 'month': 3, 'day': 13, 'event': 'Event 1'} 
-    # event_2020_2 = {'year': 2021, 'month': 7, 'day': 2, 'event': 'Event 2'} 
-    # event_2020_3 = {'year': 2021, 'month': 11, 'day': 29, 'event': 'Event 3'} 
-    # events_2020 = [event_1, event_2, event_3]
-
-    # for event in events:
-    #     fig.add_vline(x=datetime.datetime(
-    #                     event['year'],event['month'],event['day']).timestamp()*1000, 
-    #                     line_width=1, 
-    #                     line_dash="solid", 
-    #                     line_color="red", 
-    #                     annotation_text=event['event'])
+    #Add events
+    for date, event in KEY_DATES.items():
+        key_date = dt.strptime(date, '%Y-%m-%d')
+        if year == key_date.year:
+            week = datetime.date(key_date.year, key_date.month, key_date.day).isocalendar()[1]
+            fig.add_vline(x=week,
+                          line_width=1, 
+                          line_dash="solid", 
+                          line_color="red", 
+                          annotation_text=event)
 
     #Update figure
     fig.update_layout(layout)
@@ -75,5 +70,4 @@ def plot_visits_vs_2019(year):
                      range=[0, 250000000])
 
     #Return dash object
-    # return fig.show()
-    return dcc.Graph(id='visits', figure=fig)
+    return dcc.Graph(id=f'hhs_visits-{year}', figure=fig)
