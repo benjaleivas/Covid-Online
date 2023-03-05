f142ab53053d4ac90e007224fedbb31a4daf25db
Benjamin Leiva <bleiva@linux2.cs.uchicago.edu>
2023-03-05 17:47:22 -0600

    Case Study graphs ready

--- happy_app/analysis/covid_cases.py
+++ happy_app/analysis/covid_cases.py
@@ -2,31 +2,29 @@ import pandas as pd
 import datetime as dt
 from plotly_calplot import calplot  # pip install plotly-calplot
 
-def plot_covid_cases(year,metric):
+def plot_covid_cases(year):
     """
-    Creates Covid-19 calendar heatmap for chosen 'year' and 'metric'.
+    Creates Covid-19 calendar heatmap for chosen 'year'.
     Inputs:
-        - year (int): year to plot covid data for.
-        - metric (str): indicator to use, 'daily_cases' or 'log_difference'.
-    Returns (object): DCC graph of 'year' calendar map for covid's 'metric'.
+        - year (int): year to plot Covid-19 data for.
+
+    Returns (object): DCC graph .
     """
     #Load data
-    # data = pd.read_csv(f'happy_app/data/{year}_daily_covid_data.csv')
-    data = pd.read_csv(f'happy_app/data/cleaned_covid_data.csv')
+    data = pd.read_csv(f'happy_app/data/update_data/{year}_daily_covid_data.csv')
 
     #Transform data
-    data = data[['date',f'{metric}']]
+    data = data[['date','daily_cases']]
     data['date'] =  pd.to_datetime(data['date'])
-    data = data.drop(data[data['date'].dt.year != year].index)
-
 
     #Plot figure
-    fig = calplot(data,
-                  x='date',
-                  y=f'{metric}',
-                  years_title=False,
-                  showscale=True,
-                  colorscale='blues'
-                 )
+    fig = calplot(
+        data,
+        x='date',
+        y='daily_cases',
+        years_title=False,
+        showscale=True,
+        colorscale='blues'
+    )
 
-    return dcc.Graph(id=f'covid_cases-{year}-{metric}', figure=fig)
+    return dcc.Graph(id=f'covid_daily_cases-{year}', figure=fig)
--- happy_app/analysis/domain_visits.py
+++ happy_app/analysis/domain_visits.py
@@ -8,52 +8,125 @@ from happy_app.collect.utils import KEY_DATES
 
 def plot_domain_visits(key_sites):
     """
-    Plots specific domain visits for year, and its relative percentage with
-    respect to HHS visits.
+    Plots domain's cumulative visits between 2019-2022.
 
     Input:
-        - key_domains (list): list of domains to highlight in plot.
-    
+        - key_sites (list of str): list of domains to highlight in plot.
+
     Returns (object): DCC Graph.
     """
-    #Prepare data
+    # key_sites = ['cdc.gov']
+    # key_sites = ['vaccines.gov', 'vacunas.gov', 'covid.cdc.gov', 'covid.gov', 'covidtests.gov']
+
+    #Load data
     data = pd.DataFrame(columns=['year', 'week', 'domain', 'visits'])
     for year in range(2019,2022+1):
         all_sites = pd.read_csv(f'happy_app/data/update_data/{year}_all_sites.csv')
         data = pd.concat([data, all_sites], ignore_index=True)
 
     #Transform data
-    idx = pd.MultiIndex.from_product([data.year.unique(), data.week.unique(), data.domain.unique()], names=['year', 'week', 'domain'])
+    idx = pd.MultiIndex.from_product(
+        [data.year.unique(), data.week.unique(), data.domain.unique()], 
+        names=['year', 'week', 'domain']
+    )
     data = data.set_index(['year', 'week', 'domain']).reindex(idx, fill_value=0).reset_index()
     data = data.sort_values(by=['domain', 'year', 'week'], ignore_index=True)
     data['visits'] = data['visits'].astype(int)
     data['visits_cum'] = data.groupby(['domain'])['visits'].cumsum()
-    data['week_count'] = data.index % 53 + 1
-    data = data.sort_values(by=['domain', 'year', 'week'], ignore_index=True)
+    data['week_count'] = data.index % 212 + 1
+    data = data[data['domain'].isin(key_sites)]
 
     #Define layout
-    layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)', 
-                       plot_bgcolor='rgba(0,0,0,0)',
-                       title=None,
-                       title_font_family=None,
-                       title_font_color=None,
-                       font_family='Arial',
-                       font_color=None,
-                       yaxis_title=None, 
-                       xaxis_title=None)
+    layout = go.Layout(
+        paper_bgcolor='rgba(0,0,0,0)', 
+        plot_bgcolor='rgba(0,0,0,0)',
+        title=None,
+        title_font_family=None,
+        title_font_color=None,
+        font_family='Arial',
+        font_color=None,
+        font_size=14,
+        showlegend=True,
+        yaxis_title=None, 
+        xaxis_title=None,
+        xaxis=dict(
+            tickmode = 'array',
+            tickvals = [1, 9, 22, 35, \
+                        54, 63, 77, 90, \
+                        107, 117, 130, 143, \
+                        160, 170, 183, 196],
+            ticktext = ['2019', 'Mar',  'Jun', 'Sep', \
+                        '2020', 'Mar',  'Jun', 'Sep', \
+                        '2021', 'Mar',  'Jun', 'Sep', \
+                        '2022', 'Mar',  'Jun', 'Sep']
+            )
+    )
 
-    #Define lines formats
-    line_color_all_sites = ['#C1CDCD']
-    line_colors_key_sites = ['red', 'blue', 'green', 'yellow', 'purple', 'black']
-    line_names = key_sites
+    #Define line colors and y-axis range
+    if len(key_sites) == 1:
+        y_range = 5500000000
+        line_dict= {key_sites[0]:'solid'}
+        color_dict = {key_sites[0]: '#1E90FF'}
+    if len(key_sites) == 5:
+        y_range = 160000000
+        line_types = ['solid', 'dash', 'solid', 'dash', 'solid']
+        line_colors = ['#9400D3', '#9400D3', '#7bccc4', '#7bccc4', '#0868ac']
+        line_dict = dict()
+        color_dict = dict()
+        for i in range(len(key_sites)):
+            line_dict[key_sites[i]] = line_types[i]
+            color_dict[key_sites[i]] = line_colors[i]
 
     #Create figure
     fig = go.Figure()
-    for domain in data.domain.unique():
-        fig.add_trace(go.Scatter(x = datasets[idx]['week'],
-                                 y = datasets[idx]['visits'],
-                                 mode='lines',
-                                 name=line_names[idx],
-                                 line=dict(shape='linear', 
-                                           color=line_colors[idx], 
-                                           dash=line_types[idx])))
+    for site in key_sites:
+        line_color = color_dict[site]
+        subset = data[data['domain'] == site]
+        fig.add_trace(
+            go.Scatter(
+                x = subset['week_count'],
+                y = subset['visits_cum'],
+                mode='lines',
+                name=site,
+                line=dict(
+                    shape='linear',
+                    width=2,
+                    color=color_dict[site],
+                    dash=line_dict[site]
+                )
+            )
+        )
+
+    #Add events
+    events = dict()
+    if len(key_sites) == 1:
+        events['2020-03-13'] = KEY_DATES['2020-03-13']
+    else:
+        events['2020-12-11'] = 'FDA <br> Authorizes <br> Pfizer <br> Vaccine'
+        events['2021-03-08'] = 'CDC Approves Safe Gathering <br> for Vaccinated Individuals'
+        events['2022-01-19'] = 'Due to Omicron surge, Biden launches <br> online platform to order free COVID-19 tests'
+    
+    for date, event in events.items():
+        key_date = dt.strptime(date, '%Y-%m-%d')
+        diff_year_2019 = key_date.year - 2019
+        week = datetime.date(key_date.year, key_date.month, key_date.day).isocalendar()[1] + (53 * diff_year_2019)
+        fig.add_vline(
+            x=week,
+            line_width=1,
+            line_dash="solid",
+            line_color="red",
+            annotation_text=event
+        )
+
+    #Update figure
+    fig.update_layout(layout)
+    fig.update_xaxes(showticklabels=True)
+    fig.update_yaxes(
+        gridcolor="#eee",
+        griddash="solid",
+        gridwidth=0.5,
+        range=[0, y_range]
+    )
+
+    #Return dash object
+    return dcc.Graph(id=f'domain_visits-{len(key_sites)}', figure=fig)
