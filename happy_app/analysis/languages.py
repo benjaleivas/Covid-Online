f3d169f8f8a3fa8a6654181aa039631f3d42a96b
Benjamin Leiva <bleiva@linux2.cs.uchicago.edu>
2023-03-06 02:14:58 -0600

    Traffic source graph complete

--- happy_app/analysis/languages.py
+++ happy_app/analysis/languages.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from dash import dcc
+# from pywaffle import Waffle
 import plotly.express as px
 import plotly.graph_objects as go
 
@@ -7,18 +8,48 @@ import plotly.graph_objects as go
 data = pd.read_csv('happy_app/data/update_data/language.csv', usecols=['language_name', 'visits'])
 
 #Transform data
-data = data.groupby('language_name').sum()
+data = data.groupby('language_name', as_index=False).sum()
 data['percentage'] = (data.visits / data['visits'].sum())*100
 data = data.sort_values(by='visits', ascending=False)
-data.to_csv('happy_app/analysis/test.csv')
+# data = data[data.visits > 100000000]
 
-Label_per = [str(round(i*100/sum(df_coal.Value),1))+' %' for i in df_coal.Value]
+languages = data.language_name.unique()
+labels = [str(round(i*100/sum(data.visits),1))+' %' for i in data.visits]
+palette = list(sns.color_palette(palette='Paired', n_colors=len(list_country)).as_hex())
+color_language=dict(zip())
 
-fig = px.treemap(df_coal, path=[px.Constant('2022'), 'Country'],
-                 values=df_coal.Percent,
-                 color=df_coal.Country,
+fig = px.treemap(data, path=[px.Constant('2022'), 'language_name'],
+                 values=data.percentage,
+                 color=data.language_name,
                  color_discrete_map=color_country,
                  hover_name=Label_per,
                 )
 fig.update_layout(margin = dict(t=50, l=25, r=25, b=25), showlegend=True)
 fig.show()
+
+
+
+
+# data['Y'] = 1
+# data['X'] = list(range(len(data)))
+
+# label = [i+'<br>'+str(j)+'<br>'+str(k)+'%' for i,j,k in zip(data.language_name,
+#                                                             data.visits,
+#                                                             data.percentage)]
+
+# palette = list(sns.color_palette(palette='Paired', n_colors=len(list_country)).as_hex())
+
+# fig = px.scatter(data, x='X', y='Y',
+#                  color='language_name', color_discrete_sequence=palette,
+#                  size='visits', text=label, size_max=90
+#                 )
+# fig.update_layout(width=900, height=320,
+#                   margin = dict(t=50, l=0, r=0, b=0),
+#                   showlegend=False
+#                  )
+# fig.update_traces(textposition='top center')
+# fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
+# fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
+# fig.update_layout({'plot_bgcolor': 'white',
+#                    'paper_bgcolor': 'white'})
+# fig.show()
