c22e47620963cd2fa9b841f40eee1e9f1a6e29f2
Jack Gibson <jpgibson@uchicago.edu>
2023-03-06 15:53:37 -0600

    almost broke everything

--- happy_app/analysis/languages.py
+++ happy_app/analysis/languages.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from dash import dcc
-from pywaffle import Waffle
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+
 
 def plot_languages():
     """
@@ -16,52 +16,58 @@ def plot_languages():
 
     Returns (object): DCC Graph.
     """
-    #Load data
-    data = pd.read_csv('happy_app/data/update_data/language.csv', 
-                        usecols=['language_name', 'visits'])
+    # Load data
+    data = pd.read_csv(
+        "happy_app/data/update_data/language.csv", usecols=["language_name", "visits"]
+    )
 
-    #Transform data
-    data = data.groupby('language_name', as_index=False).sum()
-    data['percentage'] = round((data.visits / data['visits'].sum())*100, ndigits=1)
-    data = data.sort_values(by='visits', ascending=False)
+    # Transform data
+    data = data.groupby("language_name", as_index=False).sum()
+    data["percentage"] = round((data.visits / data["visits"].sum()) * 100, ndigits=1)
+    data = data.sort_values(by="visits", ascending=False)
     data = data[data.visits > 100000000]
-    data['visits'] = round(data.visits/100000000, ndigits=1)
+    data["visits"] = round(data.visits / 100000000, ndigits=1)
 
-    #Set features for figure
+    # Set features for figure
     languages = data.language_name.unique()
-    palette = list(sns.color_palette(palette='Paired', n_colors=len(languages)).as_hex())
-    #labels = [str(round(i*100/sum(data.visits),1))+' %' for i in data.visits]
+    palette = list(
+        sns.color_palette(palette="Paired", n_colors=len(languages)).as_hex()
+    )
+    # labels = [str(round(i*100/sum(data.visits),1))+' %' for i in data.visits]
 
-    label = [i+'<br>'+str(j)+"M"+'<br>'+str(k)+'%' for i,j,k in zip(data.language_name,
-                                                            data.visits,
-                                                            data.percentage)]
+    label = [
+        i + "<br>" + str(j) + "M" + "<br>" + str(k) + "%"
+        for i, j, k in zip(data.language_name, data.visits, data.percentage)
+    ]
 
-    data['Y'] = [1]*len(data)
-    data['X'] = list(range(0,len(data)))
-    
-    
+    data["Y"] = [1] * len(data)
+    data["X"] = list(range(0, len(data)))
 
-    fig = px.scatter(data, x=data['X'], y=data['Y'],
-                 color='language_name', color_discrete_sequence=palette,
-                 size='visits', text=label, size_max=90
-                )
-    fig.update_layout(width=900, height=320,
-                    margin = dict(t=50, l=0, r=0, b=0),
-                    showlegend=False
-                    )
-    fig.update_traces(textposition='top center')
+    fig = px.scatter(
+        data,
+        x=data["X"],
+        y=data["Y"],
+        color="language_name",
+        color_discrete_sequence=palette,
+        size="visits",
+        text=label,
+        size_max=90,
+    )
+    fig.update_layout(
+        width=900, height=320, margin=dict(t=50, l=0, r=0, b=0), showlegend=False
+    )
+    fig.update_traces(textposition="top center")
     fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
     fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
-    fig.update_layout({'plot_bgcolor': 'white',
-                    'paper_bgcolor': 'white'})
-    #fig.show()
-    
-    #return data
-    
-    #Create figure
-    # fig = plt.figure(FigureClass=Waffle, 
+    fig.update_layout({"plot_bgcolor": "white", "paper_bgcolor": "white"})
+    # fig.show()
+
+    # return data
+
+    # Create figure
+    # fig = plt.figure(FigureClass=Waffle,
     #                 rows=20, columns=50,
-    #                 values=data.percentage, 
+    #                 values=data.percentage,
     #                 colors=palette,
     #                 labels=[i+' '+j for i,j in zip(data.language_name, labels)],
     #                 figsize = (15,6),
@@ -71,4 +77,4 @@ def plot_languages():
     # plt.tight_layout()
     # plt.show()
 
-    return dcc.Graph(id=f'languages', figure=fig)
+    return dcc.Graph(id=f"languages", figure=fig)
