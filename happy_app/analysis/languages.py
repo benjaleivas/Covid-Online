465b7b3345e6db8b75373400b628d447e38ca7e8
claireboyd <claire.k.boyd@gmail.com>
2023-03-06 11:13:02 -0600

    finished langauge chart

--- happy_app/analysis/languages.py
+++ happy_app/analysis/languages.py
@@ -1,55 +1,42 @@
 import pandas as pd
 from dash import dcc
-# from pywaffle import Waffle
-import plotly.express as px
-import plotly.graph_objects as go
+from pywaffle import Waffle
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-#Load data
-data = pd.read_csv('happy_app/data/update_data/language.csv', usecols=['language_name', 'visits'])
+def plot_languages():
+    """
+    Make a waffle chart of browser language for those with over 100M visits.
 
-#Transform data
-data = data.groupby('language_name', as_index=False).sum()
-data['percentage'] = (data.visits / data['visits'].sum())*100
-data = data.sort_values(by='visits', ascending=False)
-# data = data[data.visits > 100000000]
+    Design based on Towards Data Science Waffle Chart:
+        https://towardsdatascience.com/9-visualizations-to-show-proportions-or-percentages-instead-of-a-pie-chart-4e8d81617451
 
-languages = data.language_name.unique()
-labels = [str(round(i*100/sum(data.visits),1))+' %' for i in data.visits]
-palette = list(sns.color_palette(palette='Paired', n_colors=len(list_country)).as_hex())
-color_language=dict(zip())
+    Inputs: None.
 
-fig = px.treemap(data, path=[px.Constant('2022'), 'language_name'],
-                 values=data.percentage,
-                 color=data.language_name,
-                 color_discrete_map=color_country,
-                 hover_name=Label_per,
-                )
-fig.update_layout(margin = dict(t=50, l=25, r=25, b=25), showlegend=True)
-fig.show()
+    Returns (object): DCC Graph.
+    """
+    #Load data
+    data = pd.read_csv('happy_app/data/update_data/language.csv', usecols=['language_name', 'visits'])
 
+    #Transform data
+    data = data.groupby('language_name', as_index=False).sum()
+    data['percentage'] = (data.visits / data['visits'].sum())*100
+    data = data.sort_values(by='visits', ascending=False)
+    data = data[data.visits > 100000000]
 
+    languages = data.language_name.unique()
+    palette = list(sns.color_palette(palette='Paired', n_colors=len(languages)).as_hex())
+    labels = [str(round(i*100/sum(data.visits),1))+' %' for i in data.visits]
 
+    fig = plt.figure(FigureClass=Waffle, 
+                    rows=20, columns=50,
+                    values=data.percentage, 
+                    colors=palette,
+                    labels=[i+' '+j for i,j in zip(data.language_name, labels)],
+                    figsize = (15,6),
+                    legend={'loc':'upper right',
+                            'bbox_to_anchor': (1.32, 1),
+                            })
+    plt.tight_layout()
 
-# data['Y'] = 1
-# data['X'] = list(range(len(data)))
-
-# label = [i+'<br>'+str(j)+'<br>'+str(k)+'%' for i,j,k in zip(data.language_name,
-#                                                             data.visits,
-#                                                             data.percentage)]
-
-# palette = list(sns.color_palette(palette='Paired', n_colors=len(list_country)).as_hex())
-
-# fig = px.scatter(data, x='X', y='Y',
-#                  color='language_name', color_discrete_sequence=palette,
-#                  size='visits', text=label, size_max=90
-#                 )
-# fig.update_layout(width=900, height=320,
-#                   margin = dict(t=50, l=0, r=0, b=0),
-#                   showlegend=False
-#                  )
-# fig.update_traces(textposition='top center')
-# fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
-# fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
-# fig.update_layout({'plot_bgcolor': 'white',
-#                    'paper_bgcolor': 'white'})
-# fig.show()
+    return dcc.Graph(id=f'languages', figure=fig)
